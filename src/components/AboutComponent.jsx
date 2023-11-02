184c1505ea300712a02c247af07859aa303a1803
Siraphob1 <Sirapop_t@hotmail.com>
2023-11-02 21:24:47 +0700

    update word Fullstack to Software Engineer

--- src/components/AboutComponent.jsx
+++ src/components/AboutComponent.jsx
@@ -1,22 +1,50 @@
-import imageCharacter from '../image/wp4715522.webp'
-import LayoutComponent from "./LayoutComponent"
-
-
+import imageCharacter from "../image/wp4715522.webp";
+import LayoutComponent from "./LayoutComponent";
 
 const AboutComponent = () => {
   return (
-    <LayoutComponent topic={'About'} idcomponent={'about'} bgopacity={'bg-opacity-[90%]'}>
-        <div className=" flex flex-col-reverse lg:flex-row justify-between items-center mt-[2rem]">
-            <p className=" text-white lg:text-[1rem] pl-[0.5rem] text-opacity-80  mt-[2rem] lg:w-[40%] font-Incluesive-Sans">
-              <span className="pl-[1rem]">When I started traveling, I came across FullStack-Dev, and it made me realize my passion for this field.</span>
-              <br /><span className="pl-[1rem]">The protagonist in the anime acquires new knowledge and applies it to defeat powerful monsters, constantly improving to overcome new challenges.</span>
-              <br /><span className="pl-[1rem]">The concept of my favorite characters is similar to the life of a FullStack-Developer, who is constantly battling new code and errors. To overcome them, one must constantly learn and apply the knowledge gained in every situation.situation.</span>
-              <br /><span className="pl-[1rem]">I have a strong desire to become a FullStack-Developer, mainly because it combines the skills of a swordsman and a wizard, just like in an anime. The Frontend Development is like the sword, while Backend Development is like magic. When these two skills are combined, they create a powerful synergy that I find fascinating</span>
-            </p>
-            <img src={imageCharacter} alt="" className=" lg:w-[50%] rounded-3xl" />
-        </div>
-    </LayoutComponent>
-  )
-}
+    <LayoutComponent
+      topic={"About"}
+      idcomponent={"about"}
+      bgopacity={"bg-opacity-[90%]"}
+    >
+      <div className=" flex flex-col-reverse lg:flex-row justify-between items-center mt-[2rem]">
+        <div className="  text-white lg:text-[1rem] pl-[0.5rem] text-opacity-80  mt-[2rem] lg:w-[40%] font-Incluesive-Sans">
+          <p>
+            <span className="pl-[1rem]">When I started traveling,</span> I came
+            across Software-Engineer, and it made me realize my passion for this
+            field.
+          </p>
 
-export default AboutComponent
+          <p className="py-[1rem]  ">
+            <span className="pl-[1rem]">
+              The protagonist in the anime acquires new knowledge and applies it
+              to defeat powerful monsters,
+            </span>
+            constantly improving to overcome new challenges.
+          </p>
+          <p>
+            <span className="pl-[1rem]">
+              The concept of my favorite characters is similar to the life of a
+              Software-Engineer,
+            </span>
+            who is constantly battling new code and errors. To overcome them,
+            one must constantly learn and apply the knowledge gained in every
+            situation.situation.
+          </p>
+          {/*
+          <span className="pl-[1rem]">
+            I have a strong desire to become a Software-Engineer, mainly
+            because it combines the skills of a swordsman and a wizard, just
+            like in an anime. The Frontend Development is like the sword, while
+            Backend Development is like magic. When these two skills are
+            combined, they create a powerful synergy that I find fascinating
+          </span> */}
+        </div>
+        <img src={imageCharacter} alt="" className=" lg:w-[50%] rounded-3xl" />
+      </div>
+    </LayoutComponent>
+  );
+};
+
+export default AboutComponent;
--- src/components/IntroComponent.jsx
+++ src/components/IntroComponent.jsx
@@ -1,47 +1,55 @@
-import imgProfile  from '../image/profile.jpg'
+import imgProfile from "../image/profile.jpg";
 const IntroComponent = () => {
   return (
     <section className=" relative">
-            {/* Background */}
-            <div className=" w-full h-[90vh] overflow-hidden relative">
-                <div style={{backgroundImage: `url(${imgProfile})` 
-                            ,backgroundRepeat: 'no-repeat',
-                            backgroundPosition:'50% 15%'                            
-                            }}
-                        className=" w-[110%] h-[95vh] bg-cover bg-center  blur-[15px] absolute top-[-1rem] left-[-1rem] ">     
-                </div>
-            </div>
+      {/* Background */}
+      <div className=" w-full h-[90vh] overflow-hidden relative">
+        <div
+          style={{
+            backgroundImage: `url(${imgProfile})`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "50% 15%",
+          }}
+          className=" w-[110%] h-[95vh] bg-cover bg-center  blur-[15px] absolute top-[-1rem] left-[-1rem] "
+        ></div>
+      </div>
 
-            {/* Filter Background */}
-            <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-70">
+      {/* Filter Background */}
+      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-70"></div>
 
-            </div>
+      {/* Content */}
+      <div className="flex flex-col-reverse lg:flex-row items-center justify-center absolute top-0 left-0 box-border w-full h-full px-[1.5rem] ">
+        {/* Left */}
+        <section className=" text-white flex  items-center justify-center mt-[2rem] lg:mt-0 lg:mr-[2rem]  ">
+          <div className=" lg:w-[500px] ">
+            <h1 className=" text-[8vw] sm:text-[6.5vw] md:text-[6vw]  lg:text-[3rem] font-bold mb-[0.5rem]">
+              Software-Engineer
+            </h1>
+            <p className=" lg:text-[1.2rem] font-Incluesive-Sans">
+              Hi, I am
+              <span className=" text-yellow-400"> &ldquo;Top&rdquo;</span> or
+              <span className=" text-yellow-400">
+                {" "}
+                &ldquo;Siraphob Reanmanorom.&rdquo;{" "}
+              </span>
+              <br />
+              Fan of anime where the protagonist learns everything around him
+              and adapts his knowledge to fight monsters in another worlds.
+            </p>
+          </div>
+        </section>
 
-            {/* Content */}
-            <div className="flex flex-col-reverse lg:flex-row items-center justify-center absolute top-0 left-0 box-border w-full h-full px-[1.5rem] ">
-                        {/* Left */} 
-                        <section className=" text-white flex  items-center justify-center mt-[2rem] lg:mt-0 lg:mr-[2rem]  ">
-                            <div className=" lg:w-[500px] ">
-                                <h1 className=" text-[8vw] sm:text-[6.5vw] md:text-[6vw]  lg:text-[3rem] font-bold mb-[0.5rem]">
-                                    FullStack-Developer
-                                </h1>
-                                <p className=" lg:text-[1.2rem] font-Incluesive-Sans">
-                                    Hi, I am 
-                                    <span className=" text-yellow-400"> &ldquo;Top&rdquo;</span> or  
-                                    <span className=" text-yellow-400"> &ldquo;Siraphob Reanmanorom.&rdquo; </span>
-                                    <br />Fan of anime where the protagonist learns everything around him and adapts his knowledge to fight monsters in another worlds.
-                                </p>
-                            </div>
-                        </section>            
-
-                        {/* Right */}                
-                        <section>
-                            <img src={imgProfile} alt={`${imgProfile}`} className=" w-[40vw] lg:w-[400px] rounded-full" />
-                        </section>
-            </div>
+        {/* Right */}
+        <section>
+          <img
+            src={imgProfile}
+            alt={`${imgProfile}`}
+            className=" w-[40vw] lg:w-[400px] rounded-full"
+          />
+        </section>
+      </div>
     </section>
+  );
+};
 
-  )
-}
-
-export default IntroComponent
+export default IntroComponent;
